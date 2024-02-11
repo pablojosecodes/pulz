7b958ea4f21d9b0c22b95b1b0ead088ca071a73e
pablojosecodes <pablojrhansen@gmail.com>
2024-02-11 14:40:26 -0800

    -

--- lib/config.ts
+++
@@ -1,4 +0,0 @@
-export const config: { [key: string]: string | string[] | undefined } = {
-	APP_URL: 'https://stats.udara.io',
-	CORS_ALLOWED_ORIGIN: ['stats.udara.io', 'https://udara.io', 'http://localhost:3001'],
-};
--- lib/fetcher.ts
+++
@@ -1,4 +0,0 @@
-import { Fetcher } from 'swr';
-
-export const fetcher: Fetcher<any, string> = (...args) =>
-	fetch(...args).then((res) => res.json());
